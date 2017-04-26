195ee951ebd2969091cf305293ead53e5ca8d983
vanquyettran <tvq16091993tvq>
2017-04-27 03:16:34 +0700

    init

---
+++ QueryTemplate.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: User
+ * Date: 4/27/2017
+ * Time: 2:56 AM
+ */
+
+namespace vanquyet\queryTemplate;
+
+
+use yii\base\Widget;
+
+class QueryTemplate extends Widget
+{
+    const _GROUP_OPEN = '{{';
+    const _GROUP_CLOSE = '}}';
+    const _INNER_OPEN = '[[';
+    const _INNER_CLOSE = ']]';
+
+    public function init()
+    {
+        parent::init();
+
+    }
+
+    public function run()
+    {
+        parent::run();
+
+    }
+
+
+}
