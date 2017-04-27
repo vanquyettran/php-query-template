0d690ff6a07829ac088462917c125d5ee03cdc1f
vanquyettran <quyettvq@gmail.com>
2017-04-27 16:03:39 +0700

    finds and replaces template block by text

--- QueryTemplate.php
+++ QueryTemplate.php
@@ -9,6 +9,7 @@
 namespace vanquyet\queryTemplate;
 
 
+use yii\base\Exception;
 use yii\base\Widget;
 
 class QueryTemplate extends Widget
@@ -25,19 +26,9 @@ class QueryTemplate extends Widget
     public $content;
 
     /**
-     * @var array $options
-     */
-    public $options;
-
-    /**
      * @var array $functions
      */
     public $funcList;
-
-    /**
-     * @var string $newContent
-     */
-    protected $_newContent;
 
     /**
      * @inheritdoc
@@ -46,7 +37,6 @@ class QueryTemplate extends Widget
     {
         parent::init();
 
-        $this->_newContent = $this->content;
     }
 
     /**
@@ -66,18 +56,20 @@ class QueryTemplate extends Widget
     {
         // Find all template blocks
         preg_match_all(
-            "/" . preg_quote(self::__BLOCK_OPEN) . "(.*?)" . preg_quote(self::__BLOCK_CLOSE) . "/",
+            "/" . preg_quote(self::__BLOCK_OPEN) . "(.*?)" . preg_quote(self::__BLOCK_CLOSE) . "/s",
             $this->content,
             $block_matches
         );
 
+        $newContent = $this->content;
+
         // Replace each template block by computed text
         foreach ($block_matches[0] as $block) {
             $text = $this->_blockToText($block);
-            $this->_newContent = str_replace($block, $text, $this->_newContent);
+            $newContent = str_replace($block, $text, $newContent);
         }
 
-        return $this->_newContent;
+        return $newContent;
     }
 
     /**
@@ -85,25 +77,27 @@ class QueryTemplate extends Widget
      */
     protected function _blockToText($block)
     {
-        $errors = [];
         $object = null;
+        $errors = [];
 
-        $inner = ltrim(rtrim($block, self::__BLOCK_CLOSE), self::__BLOCK_OPEN);
+        $inner = substr($block, strlen(self::__BLOCK_OPEN), - strlen(self::__BLOCK_CLOSE));
         $fns = explode('.', $inner);
 
         foreach ($fns as $i => $fn) {
             $fnName = $fn;
             $fnArgs = [];
-            preg_match_all("/\((.*?)\)/", $fn, $args_matches);
-            foreach ($args_matches[0] as $j => $args) {
-                $fnName = trim(str_replace($args, '', $fnName));
-                if ($j == 0) {
-                    $fnArgs = explode(',', ltrim(rtrim($args, ')'), '('));
-                    foreach ($fnArgs as &$fnArg) {
-                        $fnArg = trim($fnArg);
-                    }
-                }
+            preg_match_all("/\((.*?)\)/s", $fn, $args_matches);
+
+            $args_matches_length = count($args_matches[1]);
+            if ($args_matches_length > 0) {
+                $args = $args_matches[1][$args_matches_length - 1];
+                $fnName = trim(str_replace("($args)", '', $fnName));
+                $fnArgs = json_decode(
+                    "[$args]", // arguments array in json
+                    true // $assoc TRUE to cast {} => [] for all arguments
+                );
             }
+
             if ($i == 0) {
                 if (!isset($this->funcList[$fnName])) {
                     $errors[] = $this->_functionDoesNotExistError($fnName);
@@ -111,8 +105,13 @@ class QueryTemplate extends Widget
                 }
 
                 // Get object via static/first function
-                $func = $this->funcList[$fnName];
-                $object = $func(...$fnArgs);
+                try {
+                    $func = $this->funcList[$fnName];
+                    $object = $func(...$fnArgs);
+                } catch (\Exception $e) {
+                    $errors[] = $e->getMessage();
+                    break;
+                }
             } else {
                 if (!is_object($object)) {
                     $errors[] = $this->_cannotGetMethodOfNonObject($fnName);
@@ -125,7 +124,15 @@ class QueryTemplate extends Widget
                 }
 
                 // Execute method of this object
-                $object = call_user_func_array([$object, $fnName], $fnArgs);
+                try {
+                    foreach ($fnArgs as &$arg) {
+                        $arg = $this->_findAndReplaceEmbeddedMethods($object, $arg);
+                    }
+                    $object = call_user_func_array([$object, $fnName], $fnArgs);
+                } catch (\Exception $e) {
+                    $errors[] = $e->getMessage();
+                    break;
+                }
             }
         }
 
@@ -140,23 +147,66 @@ class QueryTemplate extends Widget
         return $text;
     }
 
+    protected function _findAndReplaceEmbeddedMethods($owner, $text)
+    {
+        // Find all embedded methods
+        preg_match_all(
+            "/" . preg_quote(self::__EMBED_OPEN) . "(.*?)" . preg_quote(self::__EMBED_CLOSE) . "/s",
+            $text,
+            $embed_matches
+        );
+
+        $newText = $text;
+
+        // Replace each template embed by computed text
+        foreach ($embed_matches[0] as $embeddedMethod) {
+            $embeddedText = $this->_embeddedMethodToText($owner, $embeddedMethod);
+            $newText = str_replace($embeddedMethod, $embeddedText, $newText);
+        }
+
+        return $newText;
+    }
+
+    protected function _embeddedMethodToText($owner, $embeddedMethod)
+    {
+
+    }
+
+    /**
+     * @param $errors
+     * @return string
+     */
     protected function _getErrorsMessage($errors)
     {
         return "\n<!-- " . count($errors) . " error(s):\n\t" . implode("\n\t", $errors) . "\n -->\n";
     }
 
+    /**
+     * @param $fnName
+     * @return string
+     */
     protected function _functionDoesNotExistError($fnName)
     {
         return "Function \"$fnName\" does not exist.";
     }
 
+    /**
+     * @param $objName
+     * @param $fnName
+     * @return string
+     */
     protected function _methodDoesNotExistError($objName, $fnName)
     {
         return "Object \"$objName\" does not have the method \"$fnName\".";
     }
 
+    /**
+     * @param $fnName
+     * @return string
+     */
     protected function _cannotGetMethodOfNonObject($fnName)
     {
         return "Cannot get method \"$fnName\" of non-object.";
     }
+
 }
